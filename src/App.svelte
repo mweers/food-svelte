931a11c5a6ee37808c48564936c41a1ea9a84e8e
Matt Weers <yggoir@gmail.com>
2022-04-27 14:53:54 -0500

    initial push

---
+++ src/App.svelte
@@ -0,0 +1,44 @@
+<script lang=ts>
+import { text } from "svelte/internal";
+
+    let items = [
+        { i: 1, text: 'chips', amt: 283, cal: 5 },
+        { i: 2, text: 'crunch bar', amt: 4, cal: 160 },
+        { i: 3, text: 'donuts', amt: 2, cal: 300 }
+	];
+
+	function add() {
+		items = items.concat({ i: 4, text: '', amt: 1, cal: 4 });
+	}
+
+	const remove = (item: { i: number; text: string; amt: number; cal: number; }) => {
+        items = items.filter(i => i !== item)
+	};
+
+
+	// $: remaining = todos.filter(t => !t.done).length;
+
+	// $: amtSum = list.reduce((acc, list) => acc + list.amt,0)
+	// $: calSum = list.reduce((acc, list) => acc + list.cal,0)
+	$: total = items.reduce((acc, list) => acc + list.cal*list.amt,0)
+
+</script>
+
+<h1>Food Diary</h1>
+
+{#each items as item}
+	<div>
+		<input placeholder="enter item" bind:value={item.text}>
+		<input bind:value={item.amt}>
+		<input bind:value={item.cal}>
+		<button on:click={() => remove(item)}>&times;</button>
+	</div>
+{/each}
+
+<!-- <p>{amtSum}</p>
+<p>{calSum}</p> -->
+<p>{total}</p>
+
+<button on:click={add}>
+	Add item
+</button>
